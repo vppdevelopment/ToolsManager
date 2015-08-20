6d7dca698c206dcc1b718e615865bc9f52900b43
vppdevelopment <vppdevelopment@users.noreply.github.com>
2015-08-19 21:29:28 -0500

    modificando desde la web

--- ToolsManager/ToolsManager/Program.cs
+++ ToolsManager/ToolsManager/Program.cs
@@ -14,6 +14,7 @@ namespace ToolsManager
         [STAThread]
         static void Main()
         {
+            //comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DynamicForm());
